6b988603352ca50d7f98a6a2e008b067a6dc4fee
Syro4444 <56363527+Syro4444@users.noreply.github.com>
2020-03-17 00:42:20 +0100

    ajout du endpoint hello name

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 var PORT = process.env.PORT || 3000
-const express = require('express')
+const express = require("express")
 const app = express()
 
 
 
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+app.get("/", function (req, res) {
+  res.send('Quel est votre nom ?')
+})
+
+app.get("/hello:nom", function (req, res) {
+  res.send("Bonjour, "+ req.query.name+"!" )
 })
 
 
 
 
 app.listen(PORT, function () {
-  console.log('listening on port 3000!')
+  console.log("listening on port 3000!")
 })
 
