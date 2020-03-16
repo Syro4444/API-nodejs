7b73e674ade093a9888a516f5f2cf2355a75f2f0
Syro4444 <56363527+Syro4444@users.noreply.github.com>
2020-03-17 00:00:25 +0100

    ajout de procfile pour heroku #2

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
+var PORT = process.env.PORT || 3000
 const express = require('express')
 const app = express()
+
+
+
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-app.listen(3000, function () {
+
+
+
+app.listen(PORT, function () {
   console.log('listening on port 3000!')
 })
 
